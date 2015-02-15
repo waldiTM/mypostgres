69ecb26c865934011e31a959314663681ef47fbf
Bastian Blank <waldi@debian.org>
2015-02-15 22:16:32 +0100

    Try to rewrite parameters

--- mypostgres/query/__init__.py
+++ mypostgres/query/__init__.py
@@ -1,9 +1,17 @@
-from .lexer import MysqlLexer, SqlKeyword
+from .lexer import MysqlLexer, SqlKeyword, SqlParameter
 
 
 class Query:
     def SELECT(self, query, lex):
-        pass
+        ret = []
+        print(lex)
+        for i in lex:
+            if isinstance(i, SqlParameter):
+                if i == '@@version_comment':
+                    i = 'version() as "@@version_comment"'
+            ret.append(i)
+        print(ret)
+        return self.unlex(ret)
 
     def SET(self, query, lex):
         pass
@@ -19,3 +27,14 @@ class Query:
     def __call__(self, query):
         lex = self.lexer(query)
         return getattr(self, lex[0].name)(query, lex)
+
+    def unlex(self, lex):
+        ret = []
+        for i in lex:
+            if isinstance(i, str):
+                ret.append(i)
+            elif i is None:
+                ret.append(' ')
+            else:
+                ret.append(i.__sql__())
+        return ''.join(ret)
