829f23f86203541f06ba19d0144e0a1342232fd2
Bastian Blank <waldi@debian.org>
2015-02-21 11:35:00 +0100

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,23 +1,50 @@
 CREATE SCHEMA mysql_support;
 
+/* MySQL function */
 CREATE OR REPLACE FUNCTION mysql_support.database() RETURNS name
     LANGUAGE sql AS $$
         select current_schema()
     $$;
 
-CREATE OR REPLACE FUNCTION mysql_support.if(iftest boolean, iftrue character, iffalse character) RETURNS character
+CREATE OR REPLACE FUNCTION mysql_support.if(boolean, anyelement, anyelement) RETURNS anyelement
     LANGUAGE sql AS $$
-        select case when iftest then iftrue else iffalse end
+        select case when $1 then $2 else $3 end
     $$;
 
-CREATE OR REPLACE FUNCTION mysql_support.mysql_variable_show(system_ boolean, key_ text) RETURNS text
+CREATE OR REPLACE FUNCTION mysql_support.last_insert_id() RETURNS bigint
+    LANGUAGE sql AS $$
+        select pg_catalog.lastval()
+    $$;
+
+CREATE OR REPLACE FUNCTION mysql_support.last_insert_id(bigint) RETURNS bigint
     LANGUAGE plpgsql AS $$
         declare
-	    ret text;
+            ret bigint;
         begin
-            select value into ret from mysql_variable where system = system_ and key = key_;
+            perform pg_catalog.setval('mysql_sequence', $1, false);
+            select pg_catalog.nextval('mysql_sequence') into ret;
 	    return ret;
+	end;
+    $$;
+
+/* Helper functions */
+CREATE OR REPLACE FUNCTION mysql_support.mysql_variable_show(boolean, text) RETURNS text
+    LANGUAGE plpgsql AS $$
+        declare
+            ret text;
+        begin
+            select value into ret from mysql_variable where system = $1 and key = $2;
+            return ret;
         end;
     $$;
 
-
+CREATE OR REPLACE FUNCTION mysql_support.mysql_variable_set(boolean, text, text) RETURNS void
+    LANGUAGE plpgsql AS $$
+        begin
+            update mysql_variable set value = $3 where system = $1 and key = $2;
+            if found then
+                return;
+            end if;
+            insert into mysql_variable (system, key, value) values ($1, $2, $2);
+        end;
+    $$;
